9f5af2c6372a2acaff45f5980a457416ee44749a
kdf_7 <francesco.defaveri1999@gmail.com>
2024-06-20 14:12:38 +0200

    correct ANOVA table

--- testANOVA.py
+++ testANOVA.py
@@ -1,6 +1,5 @@
-import scipy.stats as stats
-from statsmodels.stats.multicomp import pairwise_tukeyhsd
-from statsmodels.stats.multicomp import MultiComparison
+import statsmodels.api as sm
+from statsmodels.formula.api import ols
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -8,9 +7,6 @@ import seaborn as sns
 
 if __name__ == '__main__':
     df:pd.DataFrame = pd.read_csv('./results/accuracy_results.csv', sep = ',', header = 0)
-    e1 = df[df['Epsilon'] == 1]['Accuracy']
-    e2 = df[df['Epsilon'] == 0.1]['Accuracy']
-    e3 = df[df['Epsilon'] == 0.01]['Accuracy']
-    e4 = df[df['Epsilon'] == 0.001]['Accuracy']
-    f, p = stats.f_oneway(e1, e2, e3, e4)
-    print(f, p)
+    mod = ols('Accuracy ~ Epsilon', data = df).fit()
+    aov_table = sm.stats.anova_lm(mod, typ=2)
+    print(aov_table)
